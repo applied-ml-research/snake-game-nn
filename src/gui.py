cf8b1c8373446965e14a0a573f57d55b7a258679
thisistrivial <g.lee13770334@gmail.com>
2021-08-02 20:23:58 -0400

    Changed game object in gui to be static to cut out redundant code

--- src/gui.py
+++ src/gui.py
@@ -8,27 +8,26 @@ EVERY = 1000//FPS
 BACKGROUND = 'white'
 SNAKE_COLOR = 'red'
 
-direction = game.STARTING_DIRECTION
+gm = None
 canvas_buffer = []
 
 def main():
-  global direction
+  global gm 
   root = tkinter.Tk()
   root.attributes('-type', 'dialog')
   canvas = tkinter.Canvas(root, bg=BACKGROUND, height=game.HEIGHT, width=game.WIDTH)
   canvas.pack()
   gm = game.Game() 
-  root.bind('<KeyPress-Up>', lambda _: set_direction(game.UP))
-  root.bind('<KeyPress-Down>', lambda _: set_direction(game.DOWN))
-  root.bind('<KeyPress-Left>', lambda _: set_direction(game.LEFT))
-  root.bind('<KeyPress-Right>', lambda _: set_direction(game.RIGHT))
-  update(root, canvas, gm)
+  root.bind('<KeyPress-Up>', lambda _: gm.set_direction(game.UP))
+  root.bind('<KeyPress-Down>', lambda _: gm.set_direction(game.DOWN))
+  root.bind('<KeyPress-Left>', lambda _: gm.set_direction(game.LEFT))
+  root.bind('<KeyPress-Right>', lambda _: gm.set_direction(game.RIGHT))
+  update(root, canvas)
   root.mainloop()
 
-def update(root, canvas, gm):
-  global direction
+def update(root, canvas):
+  global gm 
   global canvas_buffer
-  gm.set_direction(direction)
   gm.update()
   if not gm.alive:
     gm.cleanup()
@@ -39,11 +38,7 @@ def update(root, canvas, gm):
     next_buffer.append(canvas.create_rectangle(body[game.SNAKE_X] * game.BLOCK_DIM, body[game.SNAKE_Y] * game.BLOCK_DIM, (body[game.SNAKE_X] + 1) * game.BLOCK_DIM, (body[game.SNAKE_Y] + 1) * game.BLOCK_DIM, fill=SNAKE_COLOR))
   canvas.delete(*canvas_buffer)
   canvas_buffer = next_buffer
-  root.after(EVERY, update, root, canvas, gm) 
-
-def set_direction(target):
-  global direction
-  direction = target
+  root.after(EVERY, update, root, canvas) 
 
 if __name__ == '__main__':
   main()
